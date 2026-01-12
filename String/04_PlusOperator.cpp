a4857ea4605b57f453d1c9a2a5d6ed75980d50f9
Pritam Ghosh <ghoshpritam0708@gmail.com>
2026-01-12 23:25:13 +0530

    Plus_operator

---
+++ String/04_PlusOperator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s = "Pritam";
+    s = s + " Ghosh";
+    cout<<s<<endl;
+    string r = "Pritam";
+    r = " Ghosh" + r;
+        cout<<r<<endl;
+}
