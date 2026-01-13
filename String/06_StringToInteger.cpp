208b20c50a77f2467bc4351d72b650d2030664b1
Pritam Ghosh <ghoshpritam0708@gmail.com>
2026-01-13 22:35:46 +0530

    Add files via upload

---
+++ String/06_StringToInteger.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "65921984";
+    int x = stoi(s);   //string ka integer a convert kora holo.
+    cout<<x+2;   //ata akta integer print korche.
+}
