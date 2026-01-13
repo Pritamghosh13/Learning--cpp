a2e610b28896d4617ad70cc3740368c49c5a6b55
Pritam Ghosh <ghoshpritam0708@gmail.com>
2026-01-13 22:36:04 +0530

    Add files via upload

---
+++ String/07_ToStrings.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 89154;
+    string s = to_string(x);  //integer ka string a convert kora holo.
+    s = s + "pritam";  //string a aii rokom adittion kota jai.
+    cout<<s; //output akta string hobe.
+
+}
